2345e543ffe8cfbd9b8833a5d06450098f628698
Xavier Fant <fxfant@gmail.com>
2019-12-18 08:45:27 -0500

    Removed superfluous code.

--- day2-1.js
+++ day2-1.js
@@ -4,32 +4,10 @@ let program = '1,9,10,3,2,3,11,0,99,30,40,50'.split(',').map(x => parseInt(x))
 let curopcode = program[0]
 let index = 0
 
-const splitprogram = (initprogram) => {
-	let subprograms = []
-	let curset = []
-
-	for (const x in initprogram){
-		if(x % 4 == 0){
-			if(curset.length > 0)
-				subprograms.push(curset)
-
-			curset = []
-		}
-		curset.push(initprogram[x])
-	}
-
-	if((initprogram.length - 1) % 4 != 0)
-		subprograms.push(curset)
-
-	return subprograms
-}
-
 while(true){
 	if(curopcode == 99 || (curopcode != 1 && curopcode != 2))
 		break
 
-	let splitinput = splitprogram(program)
-	let input = splitinput[index]
 	let res = 0
 	let pos1 = program[index + 1]
 	let pos2 = program[index + 2]
