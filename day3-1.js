a1d0ca8507c66f37d94ed60d74986b6055aa21ca
Xavier Fant <fxfant@gmail.com>
2019-12-21 17:20:46 -0500

    conituned question

--- day3-1.js
+++ day3-1.js
@@ -21,47 +21,51 @@ const fileparser = (filepath) => {
 
 		const wiredata = JSON.parse(data)
 		const wire1coords = convertPathToCoordinates(wiredata.wire1)
+		const wire2coords = convertPathToCoordinates(wiredata.wire2)
 
-		console.log(wire1coords)
+		console.log('wire1',wire1coords)
+		console.log('wire2',wire2coords)
 	})
 }
 
-const convertPathToCoordinates = (pathlist) => {
-	let coordinates = [[0,0]]
-	let x = 0
-	let y = 0
-
-	const pathops = {
-		'U': (pathx,pathy,units) => {
+const pathops = (dir,pathx,pathy,units) => {
+	switch (dir){
+		case 'U':
 			return {
 				'x': pathx,
 				'y': pathy + units
 			}
-		},
-		'D': (pathx,pathy,units) => {
+		case 'D':
 			return {
 				'x': pathx,
 				'y': pathy - units
 			}
-		},
-		'L': (pathx,pathy,units) => {
+		case 'L':
 			return {
 				'x': pathx - units,
 				'y': pathy
 			}
-		},
-		'R': (pathx,pathy,units) => {
+		case 'R':
 			return {
 				'x': pathx + units,
 				'y': pathy
 			}
-		},
+		default:
+			console.log("Invalid input",dir)
+			process.exit(-1)
+			break
 	}
+}
+
+const convertPathToCoordinates = (pathlist) => {
+	let coordinates = []
+	let x = 0
+	let y = 0
 
 	for(const path of pathlist){
 		const pathdata = Array.from(path)
 		const dir = pathdata[0]
-		const units = pathdata.map((value,index) => {
+		const units = pathdata.map(function(value,index){
 			if(index > 0){
 				return value
 			}
@@ -71,11 +75,13 @@ const convertPathToCoordinates = (pathlist) => {
 		})
 		.join("")
 
-		// const updatedcoords = pathops[dir](x,y,parseInt(units))
-		// const dirop = pathops[dir](x,y,parseInt(units))
-		// ({x,y} = dirop(x,y,parseInt(units)))
-		// coordinates.push([x,y])
+		let newcoords = pathops(dir,x,y,parseInt(units))
+		x = newcoords.x
+		y = newcoords.y
+		coordinates.push([x,y])
 	}
+
+	return coordinates
 }
 
 fileparser('day3-1-test1.json')
