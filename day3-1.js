0d071fedb41d55faf9cf0f375b125ccc6fac8170
Xavier Fant <fxfant@gmail.com>
2019-12-22 11:24:25 -0500

    Added logic to calculate MH distance. Need to re-work logic to use the correct coordinates.

--- day3-1.js
+++ day3-1.js
@@ -25,6 +25,9 @@ const fileparser = (filepath) => {
 
 		console.log('wire1',wire1coords)
 		console.log('wire2',wire2coords)
+
+		// find shortest Manhattan distance
+		console.log('answer',getShortestLength(wire1coords,wire2coords))
 	})
 }
 
@@ -83,5 +86,117 @@ const convertPathToCoordinates = (pathlist) => {
 
 	return coordinates
 }
+
+const getShortestLength = (w1,w2) => {
+	let len
+
+	// check each point in w1
+	// vs each line segment in
+	// w2
+
+	w1.forEach(wpoint => {
+		for(let i = 0; i < w2.length-1; i++){
+			let j = i + 1
+			let compindex
+			let curlen = 0
+
+			const curcoord = w2[i]
+			const nxtcoord = w2[j]
+
+			// determine if we need to check
+			// the x or y coordinate for this
+			// pair of coordinates
+
+			if(curcoord[0] === nxtcoord[0]){
+				// in this case we've moved vertically
+				// so compare using the y coord
+				compindex = 1
+			}
+			else if(curcoord[1] === nxtcoord[1]){
+				// in this case we've moved horizontally
+				// so compare using the x coord
+				compindex = 0
+			}
+			else{
+				// in this case invalid data was
+				// passed in which indicates an
+				// error elsewhere so natrually
+				// we just terminate the program
+				// all together
+				console.log("Invalid data",w1,w2)
+				process.exit(-1)
+			}
+
+			if((curcoord[compindex] >= wpoint[compindex] && nxtcoord[compindex] <= wpoint[compindex]) || (nxtcoord[compindex] >= wpoint[compindex] && curcoord[compindex] <= wpoint[compindex])){
+				curlen = MHDist(curcoord,nxtcoord)
+
+				console.log('curcoord',curcoord)
+				console.log('nxtcoord',nxtcoord)
+				console.log('wpoint',wpoint)
+				console.log('curlen',curlen)
+
+				if(curlen > 0 && (curlen < len || len == undefined)){
+					len = curlen
+				}
+			}
+		}
+	})
+
+	// check each point in w2
+	// vs each line segment in
+	// w1
+
+	w2.forEach(wpoint => {
+		for(let i = 0; i < w1.length-1; i++){
+			let j = i + 1
+			let compindex
+			let curlen = 0
+
+			const curcoord = w1[i]
+			const nxtcoord = w1[j]
+
+			// determine if we need to check
+			// the x or y coordinate for this
+			// pair of coordinates
+
+			if(curcoord[0] === nxtcoord[0]){
+				// in this case we've moved vertically
+				// so compare using the y coord
+				compindex = 1
+			}
+			else if(curcoord[1] === nxtcoord[1]){
+				// in this case we've moved horizontally
+				// so compare using the x coord
+				compindex = 0
+			}
+			else{
+				// in this case invalid data was
+				// passed in which indicates an
+				// error elsewhere so natrually
+				// we just terminate the program
+				// all together
+				console.log("Invalid data",w1,w2)
+				process.exit(-1)
+			}
+
+			if((curcoord[compindex] >= wpoint[compindex] && nxtcoord[compindex] <= wpoint[compindex]) || (nxtcoord[compindex] >= wpoint[compindex] && curcoord[compindex] <= wpoint[compindex])){
+				curlen = MHDist(curcoord,nxtcoord)
+
+				console.log('curcoord',curcoord)
+				console.log('nxtcoord',nxtcoord)
+				console.log('wpoint',wpoint)
+				console.log('curlen',curlen)
+
+				if(curlen > 0 && (curlen < len || len == undefined)){
+					len = curlen
+				}
+			}
+		}
+	})
+
+	return len
+}
+
+const MHDist = (p,q) => {return Math.abs(p[0] - q[0]) + Math.abs(p[1] - q[1])}
 
 fileparser('day3-1-test1.json')
